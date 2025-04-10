638e67f0d5ad4a13772c9eb290979c690279eaa9
taeyoung0524 <miseoseo1@naver.com>
2025-04-10 20:19:02 +0900

    세션4

---
+++ week4/src/App.js
@@ -0,0 +1,10 @@
+import Main from "./components/section/Main";
+import React from "react";
+
+function App() {
+  return (
+    <Main name="서태영"/>
+  );
+}
+
+export default App;
---
+++ week4/src/components/section/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+export default Main
