b851141f91a911a29b23818868c314f2d8ad0cc5
taeyoung0524 <miseoseo1@naver.com>
2025-05-28 15:20:55 +0900

    Swiper progressbar 추가

--- likelion_20250522/src/components/Main_Section/Section01.jsx
+++ likelion_20250522/src/components/Main_Section/Section01.jsx
@@ -5,22 +5,59 @@ import image3 from "../../assets/img/image 4.png"
 import image4 from "../../assets/img/image 5.png"
 import image5 from "../../assets/img/image 6.png"
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay} from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Section01 = () => {
   return (
     <div className="Section01_container">
       <Swiper
-        modules={[Autoplay]}
+        modules={[Autoplay, Pagination, Navigation]}
         autoplay={{delay:3000, disableOnInteraction:false}}
         loop={true}
         slidesPerView={1}
+        pagination={{
+          type: 'progressbar', 
+        }}
+        navigation={true}
       >
-        <SwiperSlide><img src={image1} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={image2} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={image3} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={image4} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={image5} alt="" /></SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_text">
+            <h3>24 NEW 인터런</h3>
+            <button>자세히보기</button>
+          </div>
+          <img src={image1} alt="" />
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_text">
+            <h3>24 NEW 인터런</h3>
+            <button>자세히보기</button>
+          </div>
+          <img src={image2} alt="" />
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_text">
+            <h3>24 NEW 인터런</h3>
+            <button>자세히보기</button>
+          </div>
+          <img src={image3} alt="" />
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_text">
+            <h3>24 NEW 인터런</h3>
+            <button>자세히보기</button>
+          </div>
+          <img src={image4} alt="" />
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_text">
+            <h3>24 NEW 인터런</h3>
+            <button>자세히보기</button>
+          </div>
+          <img src={image5} alt="" />
+          </SwiperSlide>
       </Swiper>
     </div>
   )
