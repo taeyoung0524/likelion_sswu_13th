227c3b9f828c8c4b79313ab9d6e8ff07437f969a
taeyoung0524 <miseoseo1@naver.com>
2025-05-28 14:04:49 +0900

    8주차 과제

--- likelion_20250522/src/components/Main_Section/Section01.jsx
+++ likelion_20250522/src/components/Main_Section/Section01.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import SlideImage from "../../assets/img/image 2.png"
+import image1 from "../../assets/img/image 2.png"
+import image2 from "../../assets/img/image 3.png"
+import image3 from "../../assets/img/image 4.png"
+import image4 from "../../assets/img/image 5.png"
+import image5 from "../../assets/img/image 6.png"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 
 const Section01 = () => {
   return (
-    <div className="Section01_wrap">
-      <img src={SlideImage} alt="" />
+    <div className="Section01_container">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{delay:3000, disableOnInteraction:false}}
+        loop={true}
+        slidesPerView={1}
+      >
+        <SwiperSlide><img src={image1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={image2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={image3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={image4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={image5} alt="" /></SwiperSlide>
+      </Swiper>
     </div>
   )
 }
